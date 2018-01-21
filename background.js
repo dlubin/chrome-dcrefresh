ab945f03fec447ea5c0bab0bc6c66aa5d75ad827
David Lubin <lubin.davidg@gmail.com>
2018-01-21 17:24:01 -0500

    The icons suck but that's not important right now. The automatic refresh works now in response to a connection failure. Next, I'm going to add a settings page so you can configure the countdown to refresh. Then, I'll work on making the countdown display on the page and making clicking on the browser action icon disable the refresh.

--- background.js
+++ background.js
@@ -1,1 +1,25 @@
-console.log("TEST BACKGROUND");
+var continueAttempts = {};
+
+var callback = function(details){
+  if(details.error == "net::ERR_INTERNET_DISCONNECTED" && details.type == "main_frame"){
+    var tabId = details.tabId || -1;
+    console.log("DCR conditions met in tab " + tabId);
+    continueAttempts[tabId] = continueAttempts[tabId] == undefined ? 1 : continueAttempts[tabId]+1;
+  var limit = 5;  
+  var countdown = setInterval(function(){
+      console.log(limit);
+      if(limit <= 0){
+        chrome.tabs.reload(tabId);
+        //just in case
+        clearInterval(countdown);
+      }else{
+        limit--;
+      }
+    }, 1000);
+  }
+}
+
+var filter = { urls: ["<all_urls>"]};
+
+chrome.webRequest.onErrorOccurred.addListener(
+  callback, filter);
